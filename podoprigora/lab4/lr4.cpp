83913b9e690b023c3e85a5236e575c7f33d24cc3
PodoprigoraIvan <shtarmel@gmail.com>
2021-10-23 22:04:02 +0300

    lb4

---
+++ podoprigora/lab4/lr4.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+char s[81];
+char outstr[161];
+
+int main()
+{
+    fgets(s, 81, stdin);
+    s[strlen(s) - 1] = '\0';
+	__asm {
+		push ds
+		pop es
+		mov esi, offset s
+		mov edi, offset outstr
+		L :
+			lodsb ; в al очередной символ
+
+			cmp al, '8'; является ли введённый символ '8'
+			jne skip1
+				mov al, '1'
+				mov ah, '0'
+				stosw
+				jmp final
+			skip1:
+
+			cmp al, '9'; является ли введённый символ '9'
+			jne skip2
+				mov ah, '1'
+				mov al, '1'
+				stosw
+				jmp final
+				skip2:
+
+			stosb; кладем в выходную строку байт из al
+
+			final:
+			mov  ecx, '\0'
+			cmp  ecx, [esi]
+			je   LExit; выход из цикла, если текущий символ завершающий
+			jmp  L
+			LExit :
+	};
+    std::cout << outstr;
+	FILE* f;
+	fopen_s(&f, "out.txt", "w");
+	fwrite(outstr, sizeof(char), strlen(outstr), f);
+    return 0;
+}
