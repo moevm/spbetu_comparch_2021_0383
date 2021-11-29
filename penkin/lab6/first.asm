9a24c63abff6f9e73858b905fc130716954a0a21
Mixagit <mixa.penkin@gmail.com>
2021-11-29 18:54:23 +0300

    penkin_lab6: done

---
+++ penkin/lab6/first.asm
@@ -0,0 +1,28 @@
+.586p
+.MODEL FLAT, C
+.CODE
+PUBLIC C first
+first PROC C array: dword, arraysize: dword, res: dword, xmin: dword
+
+push esi
+push edi
+
+mov edi, array 
+mov ecx, arraysize
+mov esi, res 
+
+for_numbers:
+	mov eax, [edi]
+	sub eax, xmin
+	mov ebx, [esi + 4*eax] 
+	inc ebx 
+	mov [esi + 4*eax], ebx 
+	add edi, 4 
+	loop for_numbers
+
+pop edi
+pop esi
+
+ret
+first ENDP
+END
