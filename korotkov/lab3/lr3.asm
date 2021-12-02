ff0d5d2893d8669d03bbb1eb7d0dddea50e7ed96
Korotkov Andrew <69258214+korotkovandrew@users.noreply.github.com>
2021-12-02 06:11:48 +0300

    Korotkov lab3: fixed

--- korotkov/lab3/lr3.asm
+++ korotkov/lab3/lr3.asm
@@ -5,13 +5,12 @@ AStack  ENDS
 ;Данные программы
 DATA SEGMENT
 	;Директивы описания данных
-	a    DW 10
-	b    DW 5
-	i    DW 5
-	k    DW 4
-	i1   DW 0
-	i2   DW 0
-	temp    DW 0	; temp
+	a       DW 10
+	b       DW 5
+	i       DW 5
+	k       DW 4
+	i1      DW 0
+	i2      DW 0
 
 DATA      ENDS
 
@@ -29,20 +28,20 @@ Main PROC  FAR
 	       mov    CX, 0
             
 	       mov    cx, i
-	       mov    temp, cx
-	       add    temp, 1   ; temp = i+1
-	       shl    cx, 1     ; C = 2i
+	       mov    dx, cx
+	       add    dx, 1   ; dx = i+1
+	       shl    cx, 1   ; cx = 2i
 	       mov    bx, b
 	       cmp    a, bx
 	       jle    fun16
          
 	; a > b
 	       mov    ax, cx
-	       mov    cx, 15
-	       sub    cx, ax
+		   neg    cx
+		   add    cx, 15
 	       mov    i1, cx
-	       mov    cx, temp
-	       add    cx, temp
+	       mov    cx, dx
+	       add    cx, dx
 	       sub    cx, 4
 	       mov    i2, cx
 	       jmp    fin
@@ -52,14 +51,15 @@ Main PROC  FAR
 	       add    cx, i
 	       add    cx, 4
 	       mov    i1, cx
-	       mov    ax, temp
-	       shl    ax, 1
-	       add    ax, temp
-	       mov    cx, 5
-	       sub    cx, ax
+
+	       mov    cx, dx
+	       shl    cx, 1
+	       add    cx, dx
+		   neg    cx
+		   add    cx, 5
 	       mov    i2, cx
 	  
-	;f5
+	;  f5
 	fin:   
 	       mov    bx, k
 	       mov    ax, i1
