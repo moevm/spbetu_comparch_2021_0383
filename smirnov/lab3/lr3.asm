b1c4d88550a5bfd441c6a279e7d4c4b789f0c803
HippoMaru <hippomaru@ya.ru>
2021-12-09 14:14:17 +0300

    Smirnov_lb3: changes done

--- smirnov/lab3/lr3.asm
+++ smirnov/lab3/lr3.asm
@@ -1,9 +1,9 @@
 AStack SEGMENT  STACK
     DW 12 DUP(?)
 AStack  ENDS
-;пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+;Данные программы
 DATA      SEGMENT
-;пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+;Директивы описания данных
 a      DW    0
 b      DW    0
 i      DW    0
@@ -13,31 +13,31 @@ i2     DW    0
 
 DATA      ENDS
 
-; пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+; Код программы
 CODE      SEGMENT
       ASSUME CS:CODE, DS:DATA, SS:AStack
 
-; пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+; Головная процедура
 Main      PROC  FAR
       push  DS
       sub   AX,AX
       push  AX
       mov   AX,DATA
       mov   DS,AX
-	  mov   CX, 0
+      mov   CX, 0
 
-	  mov a, 1
-      mov b, 2
-      mov i, -2
+      mov a, 2
+      mov b, 1
+      mov i, 1
       mov k, 0
 	  
-	  ;пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ f3
+	  ;вычисление f3
 	  mov cx, i
 	  shl cx, 1
 	  mov ax, cx
-	  shl cx, 1 ; cx 4i ax 2i
-	  mov bx, b    ; bx b
-	  cmp a, bx    ; пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ a пїЅ b
+	  shl cx, 1
+	  mov bx, b    ;
+	  cmp a, bx    ; сравнение a и b
 	  jle f1second
 		neg cx
 		add cx, 7
@@ -49,9 +49,9 @@ Main      PROC  FAR
 	  f1final:
 	  mov i1, cx
 	  
-	  ;пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ f5
+	  ;вычисление f5
 	  mov cx, ax
-	  shl cx, 1 ; cx 4i ax 2i bx b
+	  shl cx, 1
 	  cmp a, bx
 	  jle f2second
 		neg cx
@@ -64,21 +64,20 @@ Main      PROC  FAR
 	  f2final:
 	  mov i2, cx
 	  
-	  ;пїЅпїЅпїЅпїЅпїЅпїЅпїЅ res
+	  ;рассчет res
 	  mov bx, k
 	  cmp bx, 0
 	  jne resSecond
-	    mov cx, i1 ; cx i1 + i2
+	    add cx, i1
 		cmp cx, 0
-		jge skip
+		jge MainFinal
 		neg cx
-		skip:
-		  jmp MainFinal
+		jmp MainFinal
 	  resSecond:
-		cmp cx, i1
+	    cmp cx, i1
 		jl MainFinal
-		  mov cx, i1
-	  MainFinal:           ; пїЅ cx пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ res
+		mov cx, i1
+	  MainFinal:           ; в cx лежит значение res
       ret
 Main      ENDP
 CODE      ENDS
