71b60f2759dd17f4acc835c478c99bd39ef18d60
Olexsa <olexsaa@mail.ru>
2021-12-16 14:45:16 +0300

    Arsentyeva lab4

--- arsentyeva/Lab4/LAB4.cpp
+++ arsentyeva/Lab4/LAB4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 
 char in_str[81];
 char out_str[161];
@@ -20,51 +21,53 @@ and the rest of the characters of the input line will be sent directly to the ou
         pop es
         mov esi, offset in_str
         mov edi, offset out_str
-        l:
-            lodsb
-            cmp al, 'Z' ; проверка на то, что 'A' <= al <= 'Z'
+        l :
+        lodsb
+            cmp al, 'Z'; проверка на то, что 'A' <= al <= 'Z'
             jg skip1
             cmp al, 'A'
             jl skip1
             mov cl, al
-            sub cl, 17  ; -65+48 - убрали разницу между 0 и A
-            cmp al, 'K'  ; K - 75, проверка на то, что 0 <= al <= 9
+            sub cl, 17; -65 + 48 - убрали разницу между 0 и A
+            cmp al, 'K'; K - 75, проверка на то, что 0 <= al <= 9
             jl ret1
-            cmp al, 'Q'  ; Q - 81, проверка на то, что 16 <= al <= 25
+            cmp al, 'Q'; Q - 81, проверка на то, что 16 <= al <= 25
             jl case3
-                sub cl, 16
-                jmp ret1
+            jmp case4
 
-            skip1:
-            cmp al, 'z'  ; проверка на то, что 'a' <= al <= 'z'
+            skip1 :
+        cmp al, 'z'; проверка на то, что 'a' <= al <= 'z'
             jg skip2
             cmp al, 'a'
             jl skip2
             mov cl, al
-            sub cl, 49  ; -97+48 - убрали разницу между 0 и a
-            cmp al, 'k'  ; k - 107, проверка на то, что 0 <= al <= 9
+            sub cl, 49; -97 + 48 - убрали разницу между 0 и a
+            cmp al, 'k'; k - 107, проверка на то, что 0 <= al <= 9
             jl ret1
-            cmp al, 'q'  ; q - 113, проверка на то, что 16 <= al <= 25
+            cmp al, 'q'; q - 113, проверка на то, что 16 <= al <= 25
             jl case3
-                sub cl, 16
-                jmp ret1
-            
-            case3:
-                sub cl, 10  ; 10 <= al <= 15
-                mov al, 49  ; '1' - 49
-                stosb
-                jmp ret1            
-            ret1:
-                mov al, cl
-                stosb
-                jmp final
-            skip2:
-                stosb  ; Если очередной символ не удовлетворяет усл. редактирования - записываем байт в строку
+            jmp case4
+
+            case3 :
+        mov cl, al
+            sub cl, 10; 10 <= al <= 15
+            jmp ret1
+            case4 :
+        mov al, 49; '1' - 49
+            stosb
+            sub cl, 16
+            jmp ret1
+            ret1 :
+        mov al, cl
+            stosb
+            jmp final
+            skip2 :
+            stosb; Если очередной символ не удовлетворяет усл.редактирования - записываем байт в строку
             final:
-                mov  ecx, '\0'
-                cmp  ecx,[esi]  ; проверка на конец строки
-                je   lExit  ; Если достигнут конец строки - выходим из цикла
-                jmp l
+        mov  ecx, '\0'
+            cmp  ecx, [esi]; проверка на конец строки
+            je   lExit; Если достигнут конец строки - выходим из цикла
+            jmp l
             lExit :
     };
     std::cout << out_str;
