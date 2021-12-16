47beeaa51ec5d3201d0c3680a54c346934a60bca
rednamgo <rednamgo@gmail.com>
2021-12-16 07:41:09 +0300

    module_1 added

---
+++ lab6/module_1.asm
@@ -0,0 +1,31 @@
+;------------1st module
+
+.586p								;directive for correct translation of 32-bit commands (pentium cpu)
+.MODEL FLAT, C
+.CODE
+PUBLIC C module_1
+
+module_1 PROC C x: dword, arr_size: dword, result: dword, xmin: dword
+
+PUSH EDI							;save initial vals
+PUSH ESI
+
+MOV EDI, x
+MOV ESI, result
+MOV ECX, arr_size
+
+lp:
+	MOV EAX, [EDI] 					;put into EAX addr of next element
+	SUB EAX, xmin 					;get its index
+	MOV EBX, [ESI + 4 * EAX]		;put the addr of result arr into EBX
+	INC EBX 						;curr index + 1
+	MOV [ESI + 4 * EAX], EBX 		;put the result into the arr
+	ADD EDI, 4 						;next element
+	JMP lp 							;loop
+
+POP ESI
+POP EDI
+
+RET
+module_1 ENDP
+END
