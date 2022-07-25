dcfd8c0ef666889f442a6249cbc1c5545d3589a0
Mikhail Efremov <jakutenshi@gmail.com>
2022-07-25 16:21:02 +0300

    fix

---
+++ tarasov/lab5/lr5.asm
@@ -0,0 +1,120 @@
+DATA SEGMENT
+        KEEP_CS DW 0 ; для хранения сегмента
+        KEEP_IP DW 0 ; и смещения вектора прерывания
+		TMP1 DW 0
+		TMP2 DW 0
+		TMP3 DW 0
+        mes   DB 'Sample text',10,13,'$'
+		mes_end   DB 'End',10,13,'$'
+
+DATA ENDS
+
+
+Stack    SEGMENT  STACK
+          DW 12 DUP(?)
+Stack    ENDS
+
+
+
+CODE      SEGMENT
+          ASSUME CS:Code, DS:DATA, SS:Stack
+
+WriteMsg PROC NEAR
+	MOV AH,9
+	int 21h
+	ret
+WriteMsg ENDP
+
+MY_INT PROC FAR
+        jmp start
+
+        ST_SS DW 0000
+        ST_AX DW 0000
+        ST_SP DW 0000
+        IStack DW 30 DUP(?)
+start:
+
+        mov ST_SP, SP
+	mov ST_AX, AX
+	mov AX, SS
+	mov ST_SS, AX
+	mov AX, Stack
+	mov SS, AX
+	mov AX, ST_AX
+	push ax
+	push ds
+	
+	MOV   DX, OFFSET mes
+	metka:
+	call WriteMsg  ; Вывод сообщения
+	loop metka ; Заданное число раз
+
+	mov di,32
+	mov ah,0
+	int 1Ah
+	mov bx,dx; счетчик с момента сброса
+Delay:
+	mov ah,0
+	int 1Ah
+	sub dx,bx
+	cmp di,dx
+	ja Delay;переход,если больше
+	
+	MOV DX, OFFSET mes_end ;Вывод сообщения о завершении обработчика
+	MOV AH,9
+	int 21h
+	pop dx
+        pop ax
+	mov ST_AX,AX
+	mov AX,ST_SS
+	mov SS,AX
+	mov SP,ST_SP
+	mov AX,ST_AX
+	mov al,20h
+	out 20h,al
+       
+	iret
+MY_INT ENDP
+
+
+Main      PROC  FAR
+
+        push  DS       ;\  Сохранение адреса начала PSP в стеке
+        sub   AX,AX    ; > для последующего восстановления по
+        push  AX       ;/  команде ret, завершающей процедуру.
+        mov   AX,DATA             ; Загрузка сегментного
+        mov   DS,AX               ; регистра данных.
+
+        MOV AH, 35H ; функция получения вектора
+        MOV AL, 60H ; номер вектора
+        INT 21H ; возвращает текущее значение вектора прерывания
+        MOV KEEP_IP, BX ; запоминание смещения
+        MOV KEEP_CS, ES ; и сегмента вектора прерывания
+
+        PUSH DS
+        MOV DX, OFFSET MY_INT ; смещение для процедуры в DX
+        MOV AX, SEG MY_INT ; сегмент процедуры
+        MOV DS, AX ; помещаем в DS
+        MOV AH, 25H ; функция установки вектора
+        MOV AL, 60H ; номер вектора
+        INT 21H ; меняем прерывание
+        POP DS
+		
+	mov cx, 3
+        int 60H; вызов измененного прерывания
+
+        CLI
+        PUSH DS
+        MOV DX, KEEP_IP
+        MOV AX, KEEP_CS
+        MOV DS, AX
+        MOV AH, 25H
+        MOV AL, 60H
+        INT 21H ; восстанавливаем старый вектор прерывания
+        POP DS
+        STI		
+
+        RET
+Main      ENDP
+CODE      ENDS
+          END Main
